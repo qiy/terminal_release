1ecee95a0bf1989efdf902a428241ab16cf4aaa4
qiy <>
2024-08-07 19:03:09 +0800

    提交版本

--- bJS_terminal/start_release.sh
+++ bJS_terminal/start_release.sh
@@ -16,28 +16,94 @@ status_monitor()
 	Sys_uptime=$(cat /proc/uptime | awk '{print $1}')
     echo "Current system runtime:$Sys_uptime"
 }
+
 #  ------------------------------------------------------------------
-  # @brief       loop reading device node
+# @brief  启动openvpn
 #  ------------------------------------------------------------------*/
-brctl delif brtap eth0
-brctl delif brtap enx000ec677d959
-brctl delbr brtap
+run_openvpn()
+{
+    ps -ef | grep -F "openvpn" | grep -v -F "grep" > /dev/null
+    if [ $? -ne 0 ]
+    then
+        openvpn --config /etc/openvpn/client.ovpn &
+    fi
+}
 
-#start openvpn
-openvpn --config /etc/openvpn/client.ovpn &
-sleep 10
-brctl addbr brtap
-brctl addif brtap tap0
-brctl addif brtap enx000ec677d959
-ifconfig brtap 10.8.0.2 up
-ifconfig tap0 0.0.0.0 up
+#  ------------------------------------------------------------------
+# @brief  关闭openvpn
+#  ------------------------------------------------------------------*/
+killall_openvpn()
+{
+    ps -ef | grep -F "openvpn" | grep -v -F "grep" > /dev/null
+    if [ $? -eq 0 ]
+    then
+        killall openvpn
+    fi
+}
 
+#  ------------------------------------------------------------------
+  # @brief       holdBrTap_eth0tap0
+#  ------------------------------------------------------------------*/
+holdbrTap_eth0tap0()
+{
+    value_temp=$(brctl show | grep brTap)
+    if [ "${value_temp}" == "" ];then
+		ip link add brTap type bridge
+		#ifconfig brTap 10.8.0.33 netmask 255.255.255.0 up
+		ip link set brTap up
+		# brctl stp brTap on
+	else
+	    value_temp=$(brctl show | grep tap0)
+		if [ "${value_temp}" == "" ];then
+			value_temp=$(ifconfig | grep tap0)
+			if [ "${value_temp}" != "" ];then
+				ip link set tap0 master brTap
+				ifconfig tap0 0.0.0.0
+				ifconfig brTap 10.8.0.11 netmask 255.255.255.0 up
+			fi
+		fi
+
+		value_temp=$(brctl show | grep enx000ec677d959)
+		if [ "${value_temp}" == "" ]
+		then
+			ip link set enx000ec677d959 master brTap
+			ip link set enx000ec677d959 up
+		fi
+    fi
+}
+
+#  ------------------------------------------------------------------
+  # @brief       delbrTap_eth0tap0
+#  ------------------------------------------------------------------*/
+delbrTap_eth0tap0()
+{
+    value_temp=$(brctl show | grep brTap)
+    if [ "${value_temp}" != "" ];then
+		brctl delif brTap enx000ec677d959
+		brctl delif brTap tap0		
+
+		ifconfig brTap down
+		brctl delbr brTap
+    fi
+}
+
+#  ------------------------------------------------------------------
+  # @brief       init start
+#  ------------------------------------------------------------------*/
+run_openvpn
+holdbrTap_eth0tap0
 # start smart_home
 cd /home/bJS_terminal/
 rm smart_home.log
 nice -n -20 taskset -c 2 ./smart_home 123 0 >> smart_home.log &
+
+#  ------------------------------------------------------------------
+  # @brief       loop
+#  ------------------------------------------------------------------*/
 while [ 1 ]
 do
+	run_openvpn
+	holdbrTap_eth0tap0
     status_monitor > ./run_time.log
     task_flag=$(ps -elf | grep smart_home | wc -l)
     #echo $task_flag
@@ -47,8 +113,7 @@ do
         sleep 1
         nice -n -20 taskset -c 2 ./smart_home 123 0 >> smart_home.log &
     fi  
-    sleep 5
-##    ./ws_client user_name device_type &
+    sleep 10
+    #./ws_client user_name device_type &
 done
 #*********************************************END OF FILE**********************#
-
